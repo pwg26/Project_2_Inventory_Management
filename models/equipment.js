fc8f359ea406a67393b152fe3a9156a5de8d162d
Justin Douville <jdouville8@gmail.com>
2021-03-05 00:04:05 -0500

    cleaned up handlebars partials

--- models/equipment.js
+++ models/equipment.js
@@ -18,7 +18,6 @@ module.exports = function (sequelize, DataTypes) {
     },
     is_rented: {
       type: DataTypes.BOOLEAN,
-      allowNull: false,
     },
     company_renting: {
       type: DataTypes.STRING,
