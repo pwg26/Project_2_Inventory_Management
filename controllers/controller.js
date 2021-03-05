db7157db54289cb23a7f7e8e28985fb857fb69a3
Justin Douville <jdouville8@gmail.com>
2021-03-04 23:24:06 -0500

    fixed controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,17 +6,14 @@
 // but with each route gutted and replaced with sequelize queries
 // where references to our outmoded ORM file once sat.
 var express = require("express");
-
 var router = express.Router();
 // edit model to match sequelize
 var db = require("../models/");
-
 // get route -> index
 // router.get("/", function (req, res) {
 //   // send us to the next get function instead.
 //   res.redirect("/assets");
 // });
-
 // for assets page ========================================================================
 // get route, edited to match sequelize
 router.get("/", function (req, res) {
@@ -26,25 +23,14 @@ router.get("/", function (req, res) {
     .then(function (dbEquipment) {
       // into the main index, updating the page
       var hbsObject = { equipment: dbEquipment };
-<<<<<<< HEAD
-<<<<<<< HEAD
-      return res.render("product", hbsObject);
-=======
->>>>>>> 4afa588c92fe88f15d446595277a09b5fdcf53da
-=======
-
       hbsObject.equipment = hbsObject.equipment.map((eq) => ({
         ...eq,
         is_rented: !!eq.is_rented,
       }));
-
       console.log(hbsObject.equipment);
->>>>>>> master
       return res.render("index", hbsObject);
->>>>>>> 419da970a20ecec1ca51da8647ca90e74a43029f
     });
 });
-
 // post route to create new inventory item
 router.post("/assets/create", function (req, res) {
   // edited equipment create to add in a name, description, asset value, location, and rental rate
@@ -57,15 +43,21 @@ router.post("/assets/create", function (req, res) {
       res.redirect("/");
     });
 });
-
 // put route to devour a burger
 router.put("/assets/update", function (req, res) {
   // update one piece of equipment in all
   db.Equipment.update(
     {
-      req.body
+      name: req.body.name,
+      description: req.body.description,
+      asset_value: req.body.asset_value,
+      is_rented: req.body.is_rented,
+      location: req.body.location,
+      company_renting: req.body.company_renting,
+      rental_rate: req.body.rental_rate,
+      time_checked_out: req.body.company_time_checked_out,
+      time_checked_in: req.body.time_checked_in,
     },
-
     {
       where: {
         id: req.body.id,
@@ -75,7 +67,6 @@ router.put("/assets/update", function (req, res) {
     res.redirect("/");
   });
 });
-
 router.delete("/assets/update/:id", function (req, res) {
   // delte 1 equipment entry
   db.Equipment.destroy({
@@ -86,5 +77,4 @@ router.delete("/assets/update/:id", function (req, res) {
     res.redirect("/");
   });
 });
-
 module.exports = router;
