ad0fd0ac722a06dcedfc62fb0e79e4b25207579c
Pwg21 <kcaskade@gmail.com>
2021-03-08 10:12:42 -0700

    second table in the works

--- models/equipment.js
+++ models/equipment.js
@@ -5,14 +5,6 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    asset_value: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-    },
     location: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -24,10 +16,6 @@ module.exports = function (sequelize, DataTypes) {
     company_renting: {
       type: DataTypes.STRING,
       defaultValue: "--",
-    },
-    rental_rate: {
-      type: DataTypes.DECIMAL,
-      defaultValue: null,
     },
     time_checked_out: {
       type: DataTypes.DATE,
@@ -45,38 +33,3 @@ module.exports = function (sequelize, DataTypes) {
   return Equipment;
   // default: null
 };
-// module.exports = function (sequelize, DataTypes) {
-//   let Equipment2 = sequelize.define(“Equipment2”, {
-//     name: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     description: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     asset_value: {
-//       type: DataTypes.DECIMAL,
-//       allowNull: false,
-//     },
-//     asset_quantity: {
-//       type: DataTypes.DECIMAL,
-//       allowNull: false,
-//     },
-//     rental_rate: DataTypes.DECIMAL,
-//   });
-//   return Equipment2;
-// };
-// // default: null
-// const db = require(“.”);
-// let table1 = require(“./equipment”);
-// let table2 = require(“./equipment2");
-// Equipment.associate = (models) => {
-//   // We’re saying that a Post should belong to an Author
-//   // A Post can’t be created without an Author due to the foreign key constraint
-//   Equipment.belongsTo(models.Equipment2, {
-//     foreignKey: {
-//       allowNull: false,
-//     },
-//   });
-// };
