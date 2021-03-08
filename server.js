134e6d0b9cb45bbe926b95ded1443f5c55d3fbe9
Neamen_Dobamo <Neamenbirhanu@gmail.com>
2021-03-08 10:21:39 -0700

    passport authentication not fully functioning

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var app = express();
-
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
